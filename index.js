ed9dc257bfc53a5011a43d8599abfdd2574ad7e6
Jeffrey Yeh <jeffreyyehuw@gmail.com>
2023-04-23 22:34:34 -0700

    add departments

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 require('dotenv').config();
 // Import cTable to display tables
 const cTable = require('console.table');
-
+// Import inquirer to prompt user for actions
 const inquirer = require('inquirer');
 
 // Connect to database
@@ -27,6 +27,14 @@ function runProgram() {
             message: 'What would you like to do?',
             name: 'action',
             choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role', 'Quit']
+        },
+        {
+            type: 'input',
+            message: 'What is the name of the department you would like to add?',
+            name: 'addDepartment',
+            when: function(data) {
+                return data.action === 'Add Department';
+            }
         }
     ])
     .then((data) => {
@@ -51,6 +59,16 @@ function runProgram() {
                 console.table(results);
                 runProgram();
             });
+        } else if (data.action === 'Add Department') {
+            db.query('INSERT INTO department (name) VALUES (?)', data.addDepartment, (err, result) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(`Added ${data.addDepartment} to the database.`);
+                }
+                runProgram();
+            });
+            
         }
     })
     .catch((error) => {
